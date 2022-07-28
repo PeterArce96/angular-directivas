f1840cc40405c1036da72f26d4636384b6c1fd7e
PeterArce96 <parce0496@gmail.com>
2022-07-27 20:12:46 -0500

    directiva ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DirectivaNgIfComponent } from './directiva-ng-if/directiva-ng-if.component';
+import { DirectivaNgSwitchComponent } from './directiva-ng-switch/directiva-ng-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivaNgIfComponent,
+    DirectivaNgSwitchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/directiva-ng-switch/directiva-ng-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva-ng-switch',
+  templateUrl: './directiva-ng-switch.component.html',
+  styleUrls: ['./directiva-ng-switch.component.css']
+})
+export class DirectivaNgSwitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
